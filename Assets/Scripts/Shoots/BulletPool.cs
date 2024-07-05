b040c48282042d0f33589f74c9281dd7ebcae685
Gabriel Fontes <gabrielcmfontes@gmail.com>
2024-07-04 23:03:00 -0300

    Fix bullet colors bug

--- Assets/Scripts/Shoots/BulletPool.cs
+++ Assets/Scripts/Shoots/BulletPool.cs
@@ -3,59 +3,63 @@ using UnityEngine;
 
 public class BulletPool : MonoBehaviour
 {
-	public enum BulletType{PLAYER, ENEMY};
-	public static BulletPool Instance; 
+    public enum BulletType { PLAYER, ENEMY };
+    public static BulletPool Instance;
 
-	public GameObject bulletPrefab; 
-	public int poolSize; 
+    public GameObject bulletPrefab;
+    public int poolSize;
 
-	private Queue<GameObject> bulletPool;
+    private Queue<GameObject> bulletPool;
 
-	void Awake()
-	{
-		Instance = this;
-		bulletPool = new Queue<GameObject>();
+    void Awake()
+    {
+        Instance = this;
+        bulletPool = new Queue<GameObject>();
 
-		for (int i = 0; i < poolSize; i++)
-		{
-			GameObject bullet = Instantiate(bulletPrefab);
-			bullet.SetActive(false);
-			bulletPool.Enqueue(bullet);
-		}
-	}
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.SetActive(false);
+            bulletPool.Enqueue(bullet);
+        }
+    }
 
-	public GameObject GetBullet(BulletType btype)
-	{
-		GameObject bullet;
-		if (bulletPool.Count > 0)
-		{
-			bullet = bulletPool.Dequeue();
-		}
-		else
-		{
-			bullet = Instantiate(bulletPrefab);
-		}
-		if(btype == BulletType.PLAYER)
-		{
-			bullet.transform.GetChild(0).gameObject.SetActive(true);
-			bullet.tag = "PlayerBullet";
-		}else
-		{
-			bullet.transform.GetChild(1).gameObject.SetActive(true);
-			bullet.tag = "EnemyBullet";
-		}
-		bullet.SetActive(true);
-		return bullet;
-	}
+    public GameObject GetBullet(BulletType btype)
+    {
+        GameObject bullet;
+        if (bulletPool.Count > 0)
+        {
+            bullet = bulletPool.Dequeue();
+        }
+        else
+        {
+            bullet = Instantiate(bulletPrefab);
+        }
 
-	public void ReturnBullet(GameObject bullet)
-	{
-		bullet.SetActive(false);
-		for(int i = 0; i < transform.childCount; i++)
-		{
-			transform.GetChild(i).gameObject.SetActive(false);
-		}
-		bullet.tag = "Untagged";
-		bulletPool.Enqueue(bullet);
-	}
+        if (btype == BulletType.PLAYER)
+        {
+            bullet.transform.GetChild(0).gameObject.SetActive(true);
+            bullet.tag = "PlayerBullet";
+        }
+        else
+        {
+            bullet.transform.GetChild(1).gameObject.SetActive(true);
+            bullet.tag = "EnemyBullet";
+        }
+        bullet.SetActive(true);
+        return bullet;
+    }
+
+    public void ReturnBullet(GameObject bullet)
+    {
+        bullet.SetActive(false);
+
+        foreach (Transform child in bullet.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        bullet.tag = "Untagged";
+        bulletPool.Enqueue(bullet);
+    }
 }
--- Assets/Scripts/Shoots/EnemyShooter.cs
+++ Assets/Scripts/Shoots/EnemyShooter.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class EnemyShooter : MonoBehaviour
 {
-	public Transform[] firePoints;
-	public float bulletSpeed = 20f;
-	public float timeBetweenShots = 1f;
-	
-	private float shotTimer;
-	private Transform player;
-	
-	private void Start() {
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-	}
-	
-	private void Update() {
-		shotTimer += Time.deltaTime;
-		
-		if(shotTimer >= timeBetweenShots)
-		{
-			Shoot();
-			shotTimer = 0f;
-		}
-	}
-	
-	void Shoot()
-	{
-		foreach(Transform firePoint in firePoints)
-		{
-			GameObject bullet = BulletPool.Instance.GetBullet(BulletPool.BulletType.ENEMY);
-			bullet.transform.position = firePoint.position;
-			bullet.transform.rotation = firePoint.rotation;
+    public Transform[] firePoints;
+    public float bulletSpeed = 20f;
+    public float timeBetweenShots = 1f;
 
-			Vector3 direction = (player.position - firePoint.position).normalized;
-			
-			Rigidbody rb = bullet.GetComponent<Rigidbody>();
-			rb.velocity = direction * bulletSpeed;
-		}
-	}
+    private float shotTimer;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnEnable()
+    {
+        shotTimer = 0f; 
+    }
+
+    private void Update()
+    {
+        shotTimer += Time.deltaTime;
+
+        if (shotTimer >= timeBetweenShots)
+        {
+            Shoot();
+            shotTimer = 0f;
+        }
+    }
+
+    void Shoot()
+    {
+        foreach (Transform firePoint in firePoints)
+        {
+            GameObject bullet = BulletPool.Instance.GetBullet(BulletPool.BulletType.ENEMY);
+            bullet.transform.position = firePoint.position;
+            bullet.transform.rotation = firePoint.rotation;
+
+            Vector3 direction = (player.position - firePoint.position).normalized;
+
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.velocity = direction * bulletSpeed;
+        }
+    }
 }
